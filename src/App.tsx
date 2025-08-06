41020d3f99a260a47bc632f17f7b6482e4e0314a
Nikhil Gupta <164629304+Nikhil112004@users.noreply.github.com>
2025-08-06 08:07:39 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   <Routes>
     <Route path="/dashboard" element={<Dashboard />} />
     <Route path="/signin" element={<Signin />} />
-    <Route path="/signup" element={<Signup />} />
+    <Route path="/" element={<Signup />} />
   </Routes>
   
   </BrowserRouter>
